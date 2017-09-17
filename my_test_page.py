4d3e5d29f2d0c2660dcf7efadc7cc1ad8e8b0e0a
Jiaqi <jiaqi.zhang@telecom-paristech.com>
2017-09-17 11:19:45 +0200

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch master # Your branch is up-to-date with 'origin/master'. # # Changes to be committed: #	new file:   my_test_page.py # File Creation

---
+++ my_test_page.py
@@ -0,0 +1,18 @@
+# This Python file uses the following encoding: utf-8
+import matplotlib.pyplot as plt
+
+
+def kikoolol():
+    test = "il était une fois"
+    return test
+
+
+if __name__ == "__main__":
+    test1 = kikoolol()
+    print(test1)
+    test2 = "c'est fini"
+
+    plt.plot([1, 2, 3, 4])
+    plt.ylabel('some numbers')
+    # plt.show()
+    print("c'est vraiment fini")
