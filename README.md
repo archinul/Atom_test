b9ccd3640cc7e9650c4e6076f323a74e209cece0
Jiaqi ZHANG <jiaqi.zhang@telecom-paristech.fr>
2018-11-15 21:04:43 +0000

    Theia try push

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Atom_test
 
-Hello je teste theia
+Hello je teste theia. First push!
---
+++ compute.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+class A:
+    def __init__(self):
+        self.num = 0
+        self.max = 11
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.num == self.max:
+            raise StopIteration
+        else:
+            self.num += 1
+            return self.num
+        return
+
+
+a = A()
+for i in range(12):
+    print(next(a))
+
+print(np.max(range(100)))
